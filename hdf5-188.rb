486481071af5ce7e14a82f665654a4e09ac00eae
Ben Webb <ben@salilab.org>
2012-10-20 22:07:20 -0700

    Add our custom homebrew formulas.

---
+++ hdf5-188.rb
@@ -0,0 +1,41 @@
+require 'formula'
+
+# This is just the regular hdf5 formula, but held at 1.8.8, to be consistent
+# with the version that Modeller uses. We also disable the questionably-licensed
+# szip compression.
+class Hdf5188 < Formula
+  homepage 'http://www.hdfgroup.org/HDF5'
+  url 'http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.8/src/hdf5-1.8.8.tar.bz2'
+  sha1 '1bc16883ecd631840b70857bea637a06eb0155da'
+
+  # TODO - warn that these options conflict
+  option 'enable-fortran', 'Compile Fortran bindings'
+  option 'enable-threadsafe', 'Trade performance and C++ or Fortran support for thread safety'
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --enable-production
+      --enable-debug=no
+      --disable-dependency-tracking
+      --with-zlib=/usr
+      --with-szlib=no
+      --enable-filters=all
+      --enable-static=yes
+      --enable-shared=yes
+    ]
+
+    if build.include? 'enable-threadsafe'
+      args.concat %w[--with-pthread=/usr --enable-threadsafe]
+    else
+      args << '--enable-cxx'
+      if build.include? 'enable-fortran'
+        args << '--enable-fortran'
+        ENV.fortran
+      end
+    end
+
+    system "./configure", *args
+    system "make install"
+  end
+end
