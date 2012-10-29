c45c270600573216ff9521aa74a4a36f978b5d9a
Ben Webb <ben@salilab.org>
2012-10-29 16:49:18 -0700

    Add formula to build MDT.

---
+++ mdt.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Mdt < Formula
+  homepage 'http://salilab.org/mdt/'
+  url 'http://salilab.org/mdt/5.2/mdt-5.2.tar.gz'
+  sha1 '97f2771fedef141805f25662bc8dc52cb261c460'
+
+  depends_on 'scons' => :build
+  depends_on 'swig' => :build
+  depends_on 'glib'
+  depends_on 'hdf5-188' # Need same version of HDF5 as Modeller
+
+  def install
+    system "scons", "-j #{ENV.make_jobs}",
+                    "prefix=#{prefix}",
+                    "install"
+  end
+
+  def test
+    system "scons", "test"
+  end
+end
