768dc3d9336fcf3a2125272b0a36f7088b6b8bca
Ben Webb <ben@salilab.org>
2013-01-25 13:07:13 -0800

    Add HDF5 1.8.9, for newer versions of Modeller.

---
+++ hdf5-189.rb
@@ -0,0 +1,41 @@
+require 'formula'
+
+# This is just the regular hdf5 formula, but held at 1.8.9, to be consistent
+# with the version that Modeller uses. We also disable the questionably-licensed
+# szip compression.
+class Hdf5189 < Formula
+  homepage 'http://www.hdfgroup.org/HDF5'
+  url 'http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.9/src/hdf5-1.8.9.tar.bz2'
+  sha1 '7d5e5e8caa5970c65e70a5b4ad6787efe0bf70bb'
+
+  # TODO - warn that these options conflict
+  option 'enable-fortran', 'Compile Fortran bindings'
+  option 'enable-threadsafe', 'Trade performance and C++ or Fortran support for thread safety'
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --enable-production
+      --enable-debug=no
+      --disable-dependency-tracking
+      --with-zlib=/usr
+      --with-szlib=no
+      --enable-filters=all
+      --enable-static=yes
+      --enable-shared=yes
+    ]
+
+    if build.include? 'enable-threadsafe'
+      args.concat %w[--with-pthread=/usr --enable-threadsafe]
+    else
+      args << '--enable-cxx'
+      if build.include? 'enable-fortran'
+        args << '--enable-fortran'
+        ENV.fortran
+      end
+    end
+
+    system "./configure", *args
+    system "make install"
+  end
+end
