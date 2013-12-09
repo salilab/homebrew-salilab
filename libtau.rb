cff14c16f9904addc2dd59fbf4d7c7fdf830af4d
Ben Webb <ben@salilab.org>
2013-12-09 08:35:46 -0800

    Update for OS X 10.9 support.

--- libtau.rb
+++ libtau.rb
@@ -4,7 +4,7 @@ require 'formula'
 class Libtau < Formula
   homepage 'http://salilab.org/imp/libTAU.html'
   url 'http://salilab.org/imp/libTAU/libTAU-1.0.0.zip'
-  sha1 'de3e265668cfa1cd66a19cb0595bf5f18953a052'
+  sha1 'c77a94c4657d9839df4d3b46cf78effe7a897619'
 
   option :universal
 
