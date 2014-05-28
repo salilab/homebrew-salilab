0b4160e673d81421b1cd26ac5e42ab67f85174a1
Ben Webb <ben@salilab.org>
2014-05-28 12:16:42 -0700

    Update to match new SHA1 (zip file now includes 64-bit Windows binaries).

--- libtau.rb
+++ libtau.rb
@@ -4,7 +4,7 @@ require 'formula'
 class Libtau < Formula
   homepage 'http://salilab.org/imp/libTAU.html'
   url 'http://salilab.org/imp/libTAU/libTAU-1.0.0.zip'
-  sha1 'c77a94c4657d9839df4d3b46cf78effe7a897619'
+  sha1 '861614ac6c03f82c1ee17ccb66caf975a051dc0b'
 
   option :universal
 
