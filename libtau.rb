7804b541d34c078913cfae7dd566b30006c38726
Ben Webb <ben@salilab.org>
2014-10-15 13:16:47 -0700

    Update to libTAU 1.0.1.

--- libtau.rb
+++ libtau.rb
@@ -3,8 +3,8 @@ require 'formula'
 # libTAU is needed to build the IMP MultiFit modules
 class Libtau < Formula
   homepage 'http://salilab.org/imp/libTAU.html'
-  url 'http://salilab.org/imp/libTAU/libTAU-1.0.0.zip'
-  sha1 '861614ac6c03f82c1ee17ccb66caf975a051dc0b'
+  url 'http://salilab.org/imp/libTAU/libTAU-1.0.1.zip'
+  sha1 '2ae493b3a4a65df778de731ce3167e00f660a52d'
 
   option :universal
 
