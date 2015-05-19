7e4150e5e55beda20e13d98a1575e96885eaece7
Ben Webb <ben@salilab.org>
2015-05-19 13:04:08 -0700

    Update to latest release of MDT.

--- mdt.rb
+++ mdt.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Mdt < Formula
   homepage 'http://salilab.org/mdt/'
-  url 'http://salilab.org/mdt/5.2/mdt-5.2.tar.gz'
-  sha1 '97f2771fedef141805f25662bc8dc52cb261c460'
+  url 'http://salilab.org/mdt/5.3/mdt-5.3.tar.gz'
+  sha1 'ddaed8c4ea7f28ddbe5da540e9c1ba1cbf4236e8'
 
   depends_on 'scons' => :build
   depends_on 'swig' => :build
   depends_on 'glib'
-  depends_on 'hdf5-189' # Need same version of HDF5 as Modeller
+  depends_on 'hdf5-1813' # Need same version of HDF5 as Modeller
 
   def install
     system "scons", "-j #{ENV.make_jobs}",
