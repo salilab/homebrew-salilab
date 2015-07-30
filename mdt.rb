3034e8e431e0a0c427440edc8fa744bdaf841b2e
Ben Webb <ben@salilab.org>
2015-07-30 10:29:59 -0700

    Add Python3 support.

--- mdt.rb
+++ mdt.rb
@@ -5,6 +5,9 @@ class Mdt < Formula
   url 'http://salilab.org/mdt/5.3/mdt-5.3.tar.gz'
   sha1 '9bc08fdae5247d82d4adba8ca1ba07edf4da254c'
 
+  depends_on :python => :recommended
+  depends_on :python3 => :optional
+
   depends_on 'scons' => :build
   depends_on 'swig' => :build
   depends_on 'glib'
@@ -17,6 +20,22 @@ class Mdt < Formula
                     "includepath=#{hdf5_formula.include}",
                     "libpath=#{hdf5_formula.lib}",
                     "install"
+
+    if build.with? 'python3'
+      python_version = Language::Python.major_minor_version "python3"
+      python_framework = (Formula["python3"].opt_prefix)/"Frameworks/Python.framework/Versions/#{python_version}"
+      py3_inc = "#{python_framework}/Headers"
+      py3_sitepack = "#{lib}/python#{python_version}/site-packages"
+
+      system "scons", "-j #{ENV.make_jobs}",
+                      "prefix=#{prefix}",
+                      "includepath=#{hdf5_formula.include}",
+                      "libpath=#{hdf5_formula.lib}",
+                      "python=python3",
+                      "pythoninclude=#{py3_inc}",
+                      "pythondir=#{py3_sitepack}",
+                      "install"
+    end
   end
 
   def test
