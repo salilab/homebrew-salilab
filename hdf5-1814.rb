c39977bd3fbf7d2a517c737a14f094cbc5312d4f
Ben Webb <ben@salilab.org>
2016-01-13 18:40:19 -0800

    Add bottles.

--- hdf5-1814.rb
+++ hdf5-1814.rb
@@ -10,6 +10,12 @@ class Hdf51814 < Formula
 
   keg_only "Don't interfere with the regular HDF5 formula if installed."
 
+  bottle do
+    root_url "http://salilab.org/homebrew/bottles"
+    sha256 "d331a0a171126e0b7825896592614d58799adb3fa5e9fa307071aae60f8f9eb1" => :yosemite
+    sha256 "3515a41ab10e97e21322cdda642d85096780fc10e77b1674ab59932e7a4b8626" => :el_capitan
+  end
+
   # TODO - warn that these options conflict
   option 'enable-fortran', 'Compile Fortran bindings'
   option 'enable-threadsafe', 'Trade performance and C++ or Fortran support for thread safety'
