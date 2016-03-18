b7ce53cce31dc75a29e378a5c9059ff7d26a3c8d
Ben Webb <ben@salilab.org>
2016-03-18 10:59:16 -0700

    Add support for linuxbrew.

--- ifort-runtime.rb
+++ ifort-runtime.rb
@@ -3,22 +3,33 @@ require 'formula'
 class IfortRuntime < Formula
   homepage 'http://salilab.org/modeller/'
   # Extract Intel Fortran runtime from the Modeller installer image
-  url 'http://salilab.org/modeller/9.15/modeller-9.15-mac.pax.gz'
+  url 'http://salilab.org/modeller/9.15/modeller-9.15-mac.pax.gz' if OS.mac?
+  sha256 'd1ef0b6b50680dbacc20bc3fb909d86b77026195f70dd5e0e2f07573592f1c22' if OS.mac?
+  url 'http://salilab.org/modeller/9.15/modeller-9.15.tar.gz' if OS.linux?
+  sha256 '9833eace132429abee54f2f7055a55f88ac9990cd79024f95a58d12161ca8eee' if OS.linux?
   version "1.11.0"
-  sha256 'd1ef0b6b50680dbacc20bc3fb909d86b77026195f70dd5e0e2f07573592f1c22'
 
   keg_only "Don't conflict with other Intel ifort/icc libs"
 
   def install
-    libtop = "Library/modeller-9.15/lib/mac10v4"
-    libs = ["ifcore", "imf", "intlc", "irc", "svml"]
-    libs.each do |l|
-      lib.install "#{libtop}/lib#{l}.dylib"
-      # Set path to dependent libs to Homebrew location, not that used
-      # by the Modeller .dmg installer
-      libs.each do |dep|
-        system "install_name_tool", "-change", "/#{libtop}/lib#{dep}.dylib",
-               "#{lib}/lib#{dep}.dylib", "#{lib}/lib#{l}.dylib"
+    if OS.mac?
+      libtop = "Library/modeller-9.15/lib/mac10v4"
+      libs = ["ifcore", "imf", "intlc", "irc", "svml"]
+      libs.each do |l|
+        lib.install "#{libtop}/lib#{l}.dylib"
+        # Set path to dependent libs to Homebrew location, not that used
+        # by the Modeller installer
+        libs.each do |dep|
+          system "install_name_tool", "-change", "/#{libtop}/lib#{dep}.dylib",
+                 "#{lib}/lib#{dep}.dylib", "#{lib}/lib#{l}.dylib"
+        end
+      end
+    end
+    if OS.linux?
+      libtop = "lib/x86_64-intel8"
+      libs = ["ifcore.so.5", "imf.so", "intlc.so.5", "svml.so"]
+      libs.each do |l|
+        lib.install "#{libtop}/lib#{l}"
       end
     end
   end
