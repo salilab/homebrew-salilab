abd10ef11249eaeedd414632a9b5da07ef907069
Ben Webb <ben@salilab.org>
2016-08-01 15:32:24 -0700

    Update to more recent OpenCV.

--- opencv-nogui.rb
+++ opencv-nogui.rb
@@ -1,40 +1,45 @@
-require 'formula'
-
-def which_python
-  "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
-end
-
-def site_package_dir
-  "lib/#{which_python}/site-packages"
-end
+require File.expand_path("../Requirements/cuda_requirement", __FILE__)
 
 # Since homebrew doesn't yet support optional dependencies, this is a slightly
 # modified version of opencv that disables some dependencies we don't use in
 # the Sali lab, to reduce the libraries we need to bundle with IMP.
 class OpencvNogui < Formula
   desc "Open source computer vision library"
-  homepage 'http://opencv.org/'
-  url 'http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.2/OpenCV-2.4.2.tar.bz2'
-  sha1 '96ff27b87e0f028d1d16201afebabec4e0c72367'
+  homepage "http://opencv.org/"
+  url "https://github.com/Itseez/opencv/archive/2.4.13.tar.gz"
+  sha256 "94ebcca61c30034d5fb16feab8ec12c8a868f5162d20a9f0396f0f5f6d8bbbff"
+  head "https://github.com/Itseez/opencv.git", :branch => "2.4"
 
-  option '32-bit'
-  option 'with-qt',  'Build the Qt4 backend to HighGUI'
-  option 'with-tbb', 'Enable parallel code in OpenCV using Intel TBB'
+  option "32-bit"
+  option "with-qt", "Build the Qt4 backend to HighGUI"
+  option "with-tbb", "Enable parallel code in OpenCV using Intel TBB"
 
-  depends_on 'cmake' => :build
-  depends_on 'pkg-config' => :build
-  depends_on 'numpy' => :python
+  option :cxx11
+  option :universal
 
-  depends_on 'libtiff' => :optional
+  depends_on "cmake"      => :build
   depends_on 'jpeg' => :optional
   depends_on 'qt' if build.include? 'with-qt'
+
+  depends_on "pkg-config" => :build
+
+  depends_on :python => :recommended unless OS.mac? && MacOS.version > :snow_leopard
+  depends_on "homebrew/python/numpy" => :recommended if build.with? "python"
 
   # Can also depend on ffmpeg, but this pulls in a lot of extra stuff that
   # you don't need unless you're doing video analysis, and some of it isn't
   # in Homebrew anyway. Will depend on openexr if it's installed.
 
+  def arg_switch(opt)
+    (build.with? opt) ? "ON" : "OFF"
+  end
+
   def install
-    args = std_cmake_args + %w[
+    ENV.cxx11 if build.cxx11?
+    jpeg = Formula["jpeg"]
+    dylib = OS.mac? ? "dylib" : "so"
+
+    args = std_cmake_args + %W[
       -DWITH_CUDA=OFF
       -DWITH_TIFF=1
       -DWITH_JPEG=1
@@ -48,57 +53,78 @@ class OpencvNogui < Formula
       -DBUILD_TESTS=OFF
       -DBUILD_PERF_TESTS=OFF
     ]
-    if build.build_32_bit?
+    args << "-DWITH_TBB="       + arg_switch("tbb")
+    args << "-DWITH_QT="        + arg_switch("qt")
+
+    if build.with? "python"
+      py_prefix = `python-config --prefix`.chomp
+      py_lib = OS.linux? ? `python-config --configdir`.chomp : "#{py_prefix}/lib"
+      args << "-DPYTHON_LIBRARY=#{py_lib}/libpython2.7.#{dylib}"
+      args << "-DPYTHON_INCLUDE_DIR=#{py_prefix}/include/python2.7"
+      # Make sure find_program locates system Python
+      # https://github.com/Homebrew/homebrew-science/issues/2302
+      args << "-DCMAKE_PREFIX_PATH=#{py_prefix}" if OS.mac?
+    end
+
+    if build.with? "cuda"
+      ENV["CUDA_NVCC_FLAGS"] = "-Xcompiler -stdlib=libc++; -Xlinker -stdlib=libc++"
+      args << "-DWITH_CUDA=ON"
+      args << "-DCMAKE_CXX_FLAGS=-stdlib=libc++"
+      args << "-DCUDA_GENERATION=Kepler"
+    else
+      args << "-DWITH_CUDA=OFF"
+    end
+
+    # OpenCL 1.1 is required, but Snow Leopard and older come with 1.0
+    args << "-DWITH_OPENCL=OFF" if build.without?("opencl") || MacOS.version < :lion
+
+    if build.with? "openni"
+      args << "-DWITH_OPENNI=ON"
+      # Set proper path for Homebrew's openni
+      inreplace "cmake/OpenCVFindOpenNI.cmake" do |s|
+        s.gsub! "/usr/include/ni", Formula["openni"].opt_include/"ni"
+        s.gsub! "/usr/lib", Formula["openni"].opt_lib
+      end
+    end
+
+    if build.include? "32-bit"
       args << "-DCMAKE_OSX_ARCHITECTURES=i386"
       args << "-DOPENCV_EXTRA_C_FLAGS='-arch i386 -m32'"
       args << "-DOPENCV_EXTRA_CXX_FLAGS='-arch i386 -m32'"
     end
-    args << '-DWITH_QT=ON' if build.include? 'with-qt'
-    args << '-DWITH_TBB=ON' if build.include? 'with-tbb'
 
-    # The CMake `FindPythonLibs` Module is dumber than a bag of hammers when
-    # more than one python installation is available---for example, it clings
-    # to the Header folder of the system Python Framework like a drowning
-    # sailor.
-    #
-    # This code was cribbed from the VTK formula and uses the output to
-    # `python-config` to do the job FindPythonLibs should be doing in the first
-    # place.
-    python_prefix = `python-config --prefix`.strip
-    # Python is actually a library. The libpythonX.Y.dylib points to this lib, too.
-    if File.exist? "#{python_prefix}/Python"
-      # Python was compiled with --framework:
-      args << "-DPYTHON_LIBRARY='#{python_prefix}/Python'"
-      if !MacOS::CLT.installed? and python_prefix.start_with? '/System/Library'
-        # For Xcode-only systems, the headers of system's python are inside of Xcode
-        args << "-DPYTHON_INCLUDE_DIR='#{MacOS.sdk_path}/System/Library/Frameworks/Python.framework/Versions/2.7/Headers'"
-      else
-        args << "-DPYTHON_INCLUDE_DIR='#{python_prefix}/Headers'"
-      end
-    else
-      python_lib = "#{python_prefix}/lib/lib#{which_python}"
-      if File.exists? "#{python_lib}.a"
-        args << "-DPYTHON_LIBRARY='#{python_lib}.a'"
-      else
-        args << "-DPYTHON_LIBRARY='#{python_lib}.dylib'"
-      end
-      args << "-DPYTHON_INCLUDE_DIR='#{python_prefix}/include/#{which_python}'"
+    if build.universal?
+      ENV.universal_binary
+      args << "-DCMAKE_OSX_ARCHITECTURES=#{Hardware::CPU.universal_archs.as_cmake_arch_flags}"
     end
-    args << "-DPYTHON_PACKAGES_PATH='#{lib}/#{which_python}/site-packages'"
 
-    args << '..'
-    mkdir 'macbuild' do
-      system 'cmake', *args
+    if ENV.compiler == :clang && !build.bottle?
+      args << "-DENABLE_SSSE3=ON" if Hardware::CPU.ssse3?
+      args << "-DENABLE_SSE41=ON" if Hardware::CPU.sse4?
+      args << "-DENABLE_SSE42=ON" if Hardware::CPU.sse4_2?
+      args << "-DENABLE_AVX=ON" if Hardware::CPU.avx?
+    end
+
+    mkdir "macbuild" do
+      system "cmake", "..", *args
       system "make"
-      system "make install"
+      system "make", "install"
     end
   end
 
-  def caveats; <<-EOS.undent
-    The OpenCV Python module will not work until you edit your PYTHONPATH like so:
-      export PYTHONPATH="#{HOMEBREW_PREFIX}/#{site_package_dir}:$PYTHONPATH"
-
-    To make this permanent, put it in your shell's profile (e.g. ~/.profile).
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <opencv/cv.h>
+      #include <iostream>
+      int main()
+      {
+        std::cout << CV_VERSION << std::endl;
+        return 0;
+      }
     EOS
+    system ENV.cxx, "test.cpp", "-I#{include}", "-L#{lib}", "-o", "test"
+    assert_equal `./test`.strip, version.to_s
+
+    assert_match version.to_s, shell_output("python -c 'import cv2; print(cv2.__version__)'")
   end
 end
