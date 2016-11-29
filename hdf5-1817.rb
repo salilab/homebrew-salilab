58e26ac2382e7cd729c2c77942cb548f4b7bb7ad
Ben Webb <ben@salilab.org>
2016-11-29 15:56:08 -0800

    Add HDF5 1.8.17.

---
+++ hdf5-1817.rb
@@ -0,0 +1,48 @@
+require 'formula'
+
+# This is just the regular hdf5 formula, but held at 1.8.17, to be consistent
+# with the version that Modeller uses. We also disable the questionably-licensed
+# szip compression.
+class Hdf51817 < Formula
+  desc "File format designed to store large amounts of data"
+  homepage 'http://www.hdfgroup.org/HDF5'
+  url 'https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.17/src/hdf5-1.8.17.tar.bz2'
+  sha256 'fc35dd8fd8d398de6b525b27cc111c21fc79795ad6db1b1f12cb15ed1ee8486a'
+
+  keg_only "Don't interfere with the regular HDF5 formula if installed."
+
+  bottle do
+    root_url "http://salilab.org/homebrew/bottles"
+  end
+
+  # TODO - warn that these options conflict
+  option 'enable-fortran', 'Compile Fortran bindings'
+  option 'enable-threadsafe', 'Trade performance and C++ or Fortran support for thread safety'
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --enable-production
+      --enable-debug=no
+      --disable-dependency-tracking
+      --with-zlib=/usr
+      --with-szlib=no
+      --enable-filters=all
+      --enable-static=yes
+      --enable-shared=yes
+    ]
+
+    if build.include? 'enable-threadsafe'
+      args.concat %w[--with-pthread=/usr --enable-threadsafe]
+    else
+      args << '--enable-cxx'
+      if build.include? 'enable-fortran'
+        args << '--enable-fortran'
+        ENV.fortran
+      end
+    end
+
+    system "./configure", *args
+    system "make install"
+  end
+end
