b73b194e1b3399a36ae5d795b37f2baf2094892f
Ben Webb <ben@salilab.org>
2017-04-13 10:38:32 -0700

    Rebuild against newer CGAL.

--- imp.rb
+++ imp.rb
@@ -5,12 +5,13 @@ class Imp < Formula
   homepage 'https://integrativemodeling.org/'
   url 'https://integrativemodeling.org/2.7.0/download/imp-2.7.0.tar.gz'
   sha256 '877af254567051c78317ea94730dfff742aa56d21aefd71edf14a3f4b153b036'
+  revision 1
 
   bottle do
     root_url "https://integrativemodeling.org/2.7.0/download/homebrew"
-    sha256 "09d39fd42fa6111d43d79524ca9d36f8b9904d6e28ccae0c5e7f320d8a1b0afc" => :yosemite
-    sha256 "736d2e5fba242655bbe787011c32f6abae10112e3347b9667da0fffdcaaf8175" => :el_capitan
-    sha256 "cd76535f62c2587cca853843e2972e84038659a0ebe5adb5cc0597c66117e317" => :sierra
+    sha256 "d7d1a9bb6086151359dcd2fb420e0c86ae580be65a1bc4d4cd8099f7270868a1" => :yosemite
+    sha256 "b8baf52c4aafbc181d605defc79eefba1c99e39194c378f9fd9fb74a3bd631bc" => :el_capitan
+    sha256 "5941d8ab37b603c0024569456e4e67d7efb7f01cf41d703b69dce21698c4e09e" => :sierra
   end
 
   depends_on 'cmake' => :build
