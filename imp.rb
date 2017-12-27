d9fb4487fd5415c8a50335bc06c9019e844c3f18
Ben Webb <ben@salilab.org>
2017-12-26 21:39:29 -0800

    Rebuild for latest OpenCV

--- imp.rb
+++ imp.rb
@@ -5,7 +5,7 @@ class Imp < Formula
   homepage 'https://integrativemodeling.org/'
   url 'https://integrativemodeling.org/2.8.0/download/imp-2.8.0.tar.gz'
   sha256 '83a23c56f0be9de8900d0edd3978eb8c2637e6d5086f7ef7e2cd61f0b7a5aa80'
-  revision 8
+  revision 9
 
   # Fix to work with latest CGAL (4.11)
   patch do
@@ -19,9 +19,9 @@ class Imp < Formula
 
   bottle do
     root_url "https://integrativemodeling.org/2.8.0/download/homebrew"
-    sha256 "122e2e390a7941263c3aa0e6b30a6a1f87cf95d90c1618b4740d8736afb448ca" => :yosemite
-    sha256 "f31c5efa625d37d482351ffd3a58c943199500d7fdeb7ca3eb92bb92b65e6ac5" => :el_capitan
-    sha256 "79408e70f84d1e2f5dbd1bd7ad8601dc7f04573c113ca813105ff5234ec15084" => :high_sierra
+    sha256 "b361e808e6ab5fb684a40eaf149bba5e5ca1a76a71ae5fa73d4ec78a53c40d8b" => :yosemite
+    sha256 "b0b9c729e473034a3973e67ad39b96f39dcce500ed669edfcac30cd30346e9c8" => :el_capitan
+    sha256 "bc34e3d3ee780b18a288e3cb8d3fd91a4ccee6e3624e060d55b8ee35209c5e97" => :high_sierra
   end
 
   depends_on 'cmake' => :build
