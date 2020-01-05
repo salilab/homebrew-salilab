e558f28b1172909cdbd7d1191c946518ac2e40fb
Ben Webb <ben@salilab.org>
2020-01-04 20:44:02 -0800

    Rebuild against latest boost and opencv

--- imp.rb
+++ imp.rb
@@ -5,13 +5,20 @@ class Imp < Formula
   homepage 'https://integrativemodeling.org/'
   url 'https://integrativemodeling.org/2.12.0/download/imp-2.12.0.tar.gz'
   sha256 '4e7a3103773961d381cf61242060d1d6ba986d971d06fa9ce151adb00de5e837'
+  revision 1
+
+  # Work around boost::betweenness_centrality_clustering compile error
+  patch do
+    url "https://github.com/salilab/imp/commit/5d494e7ba4769343d9add281eb2a658b1b4c6d2f.diff?full_index=1"
+    sha256 "27b94702a6cf2dd45aa9306af7e1980fea988b8419e2b6367c0e5922885e6152"
+  end
 
   bottle do
     root_url "https://dl.bintray.com/salilab/homebrew"
-    sha256 "6f8dbb307f5e969ab8338273151a12a160711b3df8e8808e4d7308ed3840ef53" => :catalina
-    sha256 "51e9dd43f71d09abd64149ad95f4684136622a77cb6714ad7c9592d50599304d" => :mojave
-    sha256 "cf7f2b59d468600b3fb25eb5e143ca106b847a2b6910ad561e7e4acf32470399" => :high_sierra
-    sha256 "7440e59e6a80be04d8a5f475e7e28f6b30a0fd337b37be61e00fa0636ea01e08" => :sierra
+    sha256 "4fba5c98e893cc551a348f70a98cb54b4b7b50ed7eb46ff2c02ea664781d83fa" => :sierra
+    sha256 "917094f9dd830a88c8eabd96bef922efab0875ae41091e2f47a48ced246db127" => :high_sierra
+    sha256 "f3ea5d5a6271ada2f9af264924a2babdae6d6d886445eb6f8c46682a2dd3a155" => :catalina
+    sha256 "89848178d5017f4feb3cb1653784afd89956501ddfcb2d1b69716af8f737198a" => :mojave
   end
 
   depends_on 'cmake' => :build
