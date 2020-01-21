10f29eb7195afe04c61bbeaa997cc55c2c768753
Ben Webb <ben@salilab.org>
2020-01-21 08:32:31 -0800

    Rebuild against latest HDF5

--- imp.rb
+++ imp.rb
@@ -5,7 +5,7 @@ class Imp < Formula
   homepage 'https://integrativemodeling.org/'
   url 'https://integrativemodeling.org/2.12.0/download/imp-2.12.0.tar.gz'
   sha256 '4e7a3103773961d381cf61242060d1d6ba986d971d06fa9ce151adb00de5e837'
-  revision 1
+  revision 2
 
   # Work around boost::betweenness_centrality_clustering compile error
   patch do
@@ -13,12 +13,18 @@ class Imp < Formula
     sha256 "27b94702a6cf2dd45aa9306af7e1980fea988b8419e2b6367c0e5922885e6152"
   end
 
+  # Fix to build with HDF5 1.10.6
+  patch do
+    url "https://github.com/salilab/imp/commit/0eb1fac37aca2a41e548ba16d5ffd6e469a55460.diff?full_index=1"
+    sha256 "bbc158906ded645b37b8e06a3faf4cebabd91cdad36002f541413b98bd0e9018"
+  end
+
   bottle do
     root_url "https://dl.bintray.com/salilab/homebrew"
-    sha256 "4fba5c98e893cc551a348f70a98cb54b4b7b50ed7eb46ff2c02ea664781d83fa" => :sierra
-    sha256 "917094f9dd830a88c8eabd96bef922efab0875ae41091e2f47a48ced246db127" => :high_sierra
-    sha256 "f3ea5d5a6271ada2f9af264924a2babdae6d6d886445eb6f8c46682a2dd3a155" => :catalina
-    sha256 "89848178d5017f4feb3cb1653784afd89956501ddfcb2d1b69716af8f737198a" => :mojave
+    sha256 "d561e57c3c5b2fd9ea34582719c2e53194d8d35851374fa60a9bf21535e0d567" => :sierra
+    sha256 "c73565429e69d4873a1771699f33202cd368f0fdcddbc6263a3c3c8a55691c36" => :high_sierra
+    sha256 "fb88ac325202a1205ba1a869d18a50501af8b5aea8aa6794431c9589212ced2e" => :mojave
+    sha256 "84a2f08ed4518526c0a846cf72f873dfe46ab266a71cbcf17b228f197955ce5b" => :catalina
   end
 
   depends_on 'cmake' => :build
