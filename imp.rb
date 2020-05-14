d17c5bdf58da5d3258fa297707a148f35d99c370
Ben Webb <ben@salilab.org>
2020-05-14 10:08:59 -0700

    Update for IMP 2.13.0

--- imp.rb
+++ imp.rb
@@ -3,40 +3,27 @@ require 'formula'
 class Imp < Formula
   desc "The Integrative Modeling Platform"
   homepage 'https://integrativemodeling.org/'
-  url 'https://integrativemodeling.org/2.12.0/download/imp-2.12.0.tar.gz'
-  sha256 '4e7a3103773961d381cf61242060d1d6ba986d971d06fa9ce151adb00de5e837'
-  revision 5
+  url 'https://integrativemodeling.org/2.13.0/download/imp-2.13.0.tar.gz'
+  sha256 '528aeed272e35d79028af0e215a41c086c09782cef59ee3f983d52bff8653bfc'
 
-  # Work around boost::betweenness_centrality_clustering compile error
+  # Make sure each module has __version__
   patch do
-    url "https://github.com/salilab/imp/commit/5d494e7ba4769343d9add281eb2a658b1b4c6d2f.diff?full_index=1"
-    sha256 "27b94702a6cf2dd45aa9306af7e1980fea988b8419e2b6367c0e5922885e6152"
-  end
-
-  # Fix to build with HDF5 1.10.6
-  patch do
-    url "https://github.com/salilab/imp/commit/0eb1fac37aca2a41e548ba16d5ffd6e469a55460.diff?full_index=1"
-    sha256 "bbc158906ded645b37b8e06a3faf4cebabd91cdad36002f541413b98bd0e9018"
-  end
-
-  # Fix to build with HDF5 1.12
-  patch do
-    url "https://github.com/salilab/imp/commit/b5f703eb76a9ad9aed2a9684d41fead9ab4c2fbe.diff?full_index=1"
-    sha256 "10486f0809bd5b3f9fdcd4c1a5d7492d727b65dc4f422ef9e3a53b853b4dea6c"
+    url "https://github.com/salilab/imp/commit/17be5981c6b631d9aef8ac7f11739baecde10f19.diff?full_index=1"
+    sha256 "f454fca74610afe86a9468e488699158c6ca27fa3eec6906e5c92f1bc0cd8f7e"
   end
 
   bottle do
     root_url "https://dl.bintray.com/salilab/homebrew"
-    sha256 "241b275252cf2c88a9735ab1029ee1264157308ae3601cab55b0be618f971717" => :high_sierra
-    sha256 "1bb5306c03c3c040ba402fc24721eb8b5d21816fa36fc35ad18c3acaf0bb5ca4" => :mojave
-    sha256 "2c8f43629981e66ee63286cfdf5be9eda130351e431221e587b373e3ed7468e1" => :catalina
+    sha256 "3f58c25215096f89d24a747aa9cd99b13993ae69d11ce23d8295dfc4e3578d36" => :high_sierra
+    sha256 "3f40c0d65a0b278ce14e506ec1266cc766c65656ce5440d6f17af0a218e6f655" => :mojave
+    sha256 "3df62890818b6ac41f293065a524b52cd21b9bc5a378f8d133cc4b1e7a243b1d" => :catalina
   end
 
   depends_on 'cmake' => :build
   depends_on 'swig' => :build
   depends_on 'pkg-config' => :build
 
-  depends_on 'python' => :recommended
+  depends_on 'python@3.8' => :recommended
 
   depends_on 'boost'
   depends_on 'hdf5'
@@ -75,8 +62,8 @@ class Imp < Formula
       end
       system "make"
       system "make", "install"
-      if build.with? 'python'
-        version = Language::Python.major_minor_version "python3"
+      if build.with? 'python@3.8'
+        version = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
         args = ["..",
                 "-DCMAKE_INSTALL_PYTHONDIR=#{lib}/python#{version}/site-packages",
                 "-DUSE_PYTHON2=off"]
@@ -85,14 +72,15 @@ class Imp < Formula
         cd bin do
           # Make sure binaries use Homebrew Python
           inreplace pybins, %r{^#!.*python.*$},
-                            "#!#{Formula["python"].opt_bin}/python3"
+                            "#!#{Formula["python@3.8"].opt_bin}/python3"
         end
       end
     end
   end
 
   test do
-    Language::Python.each_python(build) do |python, pyver|
+    pythons = [ Formula["python@3.8"].opt_bin/"python3", "python2.7" ]
+    pythons.each do |python|
       system python, "-c", "import IMP; assert(IMP.__version__ == '#{version}')"
       system python, "-c", "import IMP.em2d; assert(IMP.em2d.__version__ == '#{version}')"
       system python, "-c", "import IMP.cgal; assert(IMP.cgal.__version__ == '#{version}')"
@@ -100,11 +88,23 @@ class Imp < Formula
       system python, "-c", "import IMP.multifit; assert(IMP.multifit.__version__ == '#{version}')"
       system python, "-c", "import IMP.npctransport; assert(IMP.npctransport.__version__ == '#{version}')"
       system python, "-c", "import IMP.bayesianem; assert(IMP.bayesianem.__version__ == '#{version}')"
+      system python, "-c", "import IMP.sampcon; assert(IMP.sampcon.__version__ == '#{version}')"
       system python, "-c", "import IMP, RMF, os; name = IMP.create_temporary_file_name('assignments', '.hdf5'); root = RMF.HDF5.create_file(name); del root; os.unlink(name)"
       system python, "-c", "import IMP.mpi; assert(IMP.mpi.__version__ == '#{version}')"
     end
     system "multifit"
     system "foxs"
+  end
+
+  def caveats
+    <<~EOS
+      IMP has been built to work with the latest version of Python 3
+      supported by Homebrew, Python 3.8.
+
+      Note that currently this is not in Homebrew's default path.
+      See "brew info python@3.8" for the full path to Python 3.8 and
+      instructions on how to add it to the default PATH.
+    EOS
   end
 
 end
