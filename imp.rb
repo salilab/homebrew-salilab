add6bae2dc287f40fb80fd366a47706cd47f904b
Ben Webb <ben@salilab.org>
2022-03-31 11:55:06 -0700

    Drop Python 2, rebuild for latest HDF5
    We can't build with Python 2 on Monterey since Apple
    dropped it, so just drop Python 2 builds.
    
    homebrew downgraded HDF5 from 1.13 to 1.12, so rebuild
    against that "new" version.

--- imp.rb
+++ imp.rb
@@ -6,14 +6,14 @@ class Imp < Formula
   url "https://integrativemodeling.org/2.16.0/download/imp-2.16.0.tar.gz"
   sha256 "8593a85c729f095fe2e9dd07673a9ad7a7afd4a230ed27ab5b8158809cf61889"
   license "LGPL/GPL"
-  revision 5
+  revision 6
 
   bottle do
     root_url "https://salilab.org/homebrew/bottles"
-    sha256 arm64_monterey: "afb96b60a2443319f4ee6dd0e83ffac23af2d804815f47f4bb72c73c1f50bb4e"
-    sha256 monterey:       "0f66fe8700a7733776e6f86bcb51ef1d5a04d433b7d4f284cc99c23c7614379d"
-    sha256 big_sur:        "c023ce35c84d9e22b2ad546c1e259785b34a9c7cbd811a048ce8af41271997b5"
-    sha256 catalina:       "c56a8865f445f47d906975e40fd7f7e9ae7e7e20c6e43c7220be6d52c0c320e4"
+    sha256 arm64_monterey: "c249e6dcc5914ef4db666794212c528b0b448fb8169030ada09a488bd868ec74"
+    sha256 monterey:       "f4fc5f0c4d8d7fe1476e944f86525176d12b84b152dcb5f381f28834c9ad9b4b"
+    sha256 big_sur:        "1074ae9d589bc02f30019de3d874073da0a7106200bfc9ae6e522c898c554a6f"
+    sha256 catalina:       "4b46da288c84557156fe44d950e394726cca0392fdd6cb6236d959fd24478047"
   end
 
   depends_on "cmake" => :build
@@ -26,25 +26,25 @@ class Imp < Formula
   depends_on "hdf5"
   depends_on "open-mpi"
   depends_on "protobuf"
+  depends_on "python@3.9"
   depends_on "cgal" => :recommended
   depends_on "gsl" => :recommended
   depends_on "libtau" => :recommended
   depends_on "opencv" => :recommended
-  depends_on "python@3.9" => :recommended
 
   def install
     ENV.cxx11
-    pyver = Language::Python.major_minor_version "python2.7"
+    version = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
     args = std_cmake_args
     args << "-DIMP_DISABLED_MODULES=scratch"
     args << ".."
+    args << "-DCMAKE_INSTALL_PYTHONDIR=#{lib}/python#{version}/site-packages"
     # Otherwise linkage of _IMP_em2d.so fails on arm64 because it can't find
     # @rpath/libgcc_s.1.1.dylib
     gcclib = Formula["gcc"].lib/"gcc/11"
     args << "-DCMAKE_MODULE_LINKER_FLAGS=-L#{gcclib}"
     # Don't install in lib64 on Linux systems
     args << "-DCMAKE_INSTALL_LIBDIR=#{lib}"
-    args << "-DCMAKE_INSTALL_PYTHONDIR=#{lib}/python#{pyver}/site-packages"
     # Don't link against gperftools, even if they were found, since then the
     # bottle won't work on systems without gperftools installed
     args << "-DGPerfTools_found=0"
@@ -54,8 +54,8 @@ class Imp < Formula
     args << "-DIMP_NO_LOG4CXX=1"
     # Help cmake to find CGAL
     ENV["CGAL_DIR"] = Formula["cgal"].lib/"cmake/CGAL"
-    # Force Python 2
-    args << "-DUSE_PYTHON2=on"
+    # Force Python 3
+    args << "-DUSE_PYTHON2=off"
     mkdir "build" do
       system "cmake", *args
       pybins = []
@@ -64,25 +64,16 @@ class Imp < Formula
       end
       system "make"
       system "make", "install"
-      if build.with? "python@3.9"
-        version = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
-        args = ["..",
-                "-DCMAKE_INSTALL_PYTHONDIR=#{lib}/python#{version}/site-packages",
-                "-DUSE_PYTHON2=off",
-                "-DLog4CXX_LIBRARY=Log4CXX_LIBRARY-NOTFOUND"]
-        system "cmake", *args
-        system "make", "install"
-        cd bin do
-          # Make sure binaries use Homebrew Python
-          inreplace pybins, %r{^#!.*python.*$},
-                            "#!#{Formula["python@3.9"].opt_bin}/python3"
-        end
+      cd bin do
+        # Make sure binaries use Homebrew Python
+        inreplace pybins, %r{^#!.*python.*$},
+                          "#!#{Formula["python@3.9"].opt_bin}/python3"
       end
     end
   end
 
   test do
-    pythons = [Formula["python@3.9"].opt_bin/"python3", "python2.7"]
+    pythons = [Formula["python@3.9"].opt_bin/"python3"]
     pythons.each do |python|
       system python, "-c", "import IMP; assert(IMP.__version__ == '#{version}')"
       system python, "-c", "import IMP.em2d; assert(IMP.em2d.__version__ == '#{version}')"
