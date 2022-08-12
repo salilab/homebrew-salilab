69f7a9b06a53221a181053fc6bdc08a543c9dce5
Ben Webb <ben@salilab.org>
2022-08-12 12:49:10 -0700

    Update to Python 3.10, fix build with new CGAL

--- imp.rb
+++ imp.rb
@@ -6,14 +6,14 @@ class Imp < Formula
   url "https://integrativemodeling.org/2.17.0/download/imp-2.17.0.tar.gz"
   sha256 "2667f7a4f7b4830ba27e0d41e2cab0fc21ca22176625bfd8b2f353b283dfc8af"
   license "LGPL/GPL"
-  revision 2
+  revision 3
 
   bottle do
     root_url "https://salilab.org/homebrew/bottles"
-    sha256 arm64_monterey: "39e5fb5f52c2de3258fb44d22d7d8320cee93abd01ddd56969ec2ff5fe9e9b0e"
-    sha256 monterey:       "c790c5512201fa35825154e6743fbaa23faba3e673bfe710c6aabecdc1340031"
-    sha256 big_sur:        "680edd503dc757f47a25a4b19012b236593c75d1b56c3ecbb02aa7aa66a8a596"
-    sha256 catalina:       "db212f46cd2910db02bff9946d0f4d27172ba55ac53e5810616a22ebac5e35ac"
+    sha256 arm64_monterey: "5368163ec798105d2fe73999cb492dad1eace48e4b0814d333e56e424d9418a2"
+    sha256 monterey:       "9a8ae466a3aa464b252ed32bf0ae3f5fe68a68714fe9666369ec456398146689"
+    sha256 big_sur:        "9195c9aef23d02fedc2b5f99163c6a0ce94aeebabea1c3f6420d85500fbbb1f8"
+    sha256 catalina:       "af772c1f70b4c9095446ab49d6f2881ae0c9f340025176a8045d1278ffa38d53"
   end
 
   depends_on "cmake" => :build
@@ -26,15 +26,25 @@ class Imp < Formula
   depends_on "hdf5"
   depends_on "open-mpi"
   depends_on "protobuf"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
   depends_on "cgal" => :recommended
   depends_on "gsl" => :recommended
   depends_on "libtau" => :recommended
   depends_on "opencv" => :recommended
 
+  # Fix build with newer CGAL
+  patch do
+    url "https://github.com/salilab/imp/commit/c1522b37fb8bcf53c1d9738e30519a0a5c2b0831.patch?full_index=1"
+    sha256 "6b5bc748a393b006dcd76bda02163d5b80b573cfb07a944d50bb49ef7dc8851f"
+  end
+  patch do
+    url "https://github.com/salilab/imp/commit/806d3f182b4e8f23420f5cc6687a48836099b0c7.patch?full_index=1"
+    sha256 "c10fc7b3913725f7123cbdbd14eec57a4126e11aaf0a5e53f541e02ad36937cf"
+  end
+
   def install
     ENV.cxx11
-    version = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
+    version = Language::Python.major_minor_version Formula["python@3.10"].opt_bin/"python3.10"
     args = std_cmake_args
     args << "-DIMP_DISABLED_MODULES=scratch"
     args << ".."
@@ -67,13 +77,13 @@ class Imp < Formula
       cd bin do
         # Make sure binaries use Homebrew Python
         inreplace pybins, %r{^#!.*python.*$},
-                          "#!#{Formula["python@3.9"].opt_bin}/python3.9"
+                          "#!#{Formula["python@3.10"].opt_bin}/python3.10"
       end
     end
   end
 
   test do
-    pythons = [Formula["python@3.9"].opt_bin/"python3.9"]
+    pythons = [Formula["python@3.10"].opt_bin/"python3.10"]
     pythons.each do |python|
       system python, "-c", "import IMP; assert(IMP.__version__ == '#{version}')"
       system python, "-c", "import IMP.em2d; assert(IMP.em2d.__version__ == '#{version}')"
