46194623251c975def0691bded8ace4094f421cc
Ben Webb <ben@salilab.org>
2025-02-13 21:15:29 -0800

    Update to 2.2 release

--- ihm.rb
+++ ihm.rb
@@ -1,31 +1,21 @@
 class Ihm < Formula
   desc "Package for handling IHM mmCIF and BinaryCIF files"
   homepage "https://github.com/ihmwg/python-ihm"
-  url "https://pypi.io/packages/source/i/ihm/ihm-2.0.tar.gz"
-  sha256 "50d4fbef40138c7cabe6626a2ff25fb951256738ecce1274032feea7af9604b1"
+  url "https://pypi.io/packages/source/i/ihm/ihm-2.2.tar.gz"
+  sha256 "07967f323d1df12f81246b51b75e5910e54e8588e680be82709f0d347215a1f7"
   license "MIT"
 
   bottle do
     root_url "https://salilab.org/homebrew/bottles"
-    sha256 arm64_sequoia: "45f87759093d96f914c5ca93356ce32a35476cdf68f44b652e015b32d92b27d8"
-    sha256 arm64_sonoma:  "f9818e6694b7def7aa4b24a7b41e8845247e6ff966b5d2ff3b4d4ad19c2fdf4c"
-    sha256 arm64_ventura: "b6611a919f68e8439af8a9cc24ea98fe4f57c62679112d057738c1f279f0e1c9"
-    sha256 sequoia:       "d8325f03a0a0f9de30525a4daebed7af5bc2cefb224bfa2c37670bfb8d630de5"
-    sha256 sonoma:        "7ca7a972e3ec4e6294452ff824c037537ff32e173fe2e1591cbb7b676a8400c0"
-    sha256 ventura:       "03ef8c205b1c8158f1f878226a6c0200a7af2bf4af1e76f69d559bddd18b7ec0"
+    sha256 arm64_sequoia: "ce0c14dff5250174608435bbb04e179b6dc5f67d3cfab02b3ca1c58592e4286f"
+    sha256 arm64_sonoma:  "682959023698c645f2f7b2210d192c89c7b93ff6510c004f54889a462a9c40be"
+    sha256 arm64_ventura: "66eda3c85cac23b00a03e06fb0451b47b84776b55f5fc4343be496ddd5c2d49f"
+    sha256 sequoia:       "f4fdc3dc076d299dfe0f57cc9d48d889c44d9e47d1497769d6cee3914c143065"
+    sha256 sonoma:        "26f3e80f6e78c7828f4f8d417e6fdea8e2cd9c3cb90cd112c79436224be8206a"
+    sha256 ventura:       "dd33aead6bffb07d399a8b79f1e33e096de1f7c190803dfca12362e7b14c83d8"
   end
 
   depends_on "python@3.13"
-
-  # Include upstream BinaryCIF fixes
-  patch do
-    url "https://github.com/ihmwg/python-ihm/commit/bca8d4f24eab38ca0925df54d6df410916aa5323.patch?full_index=1"
-    sha256 "5b8bd60d5329453bb544f0a8e7aa35238661f76b57731b7dbf639b94c893f1f1"
-  end
-  patch do
-    url "https://github.com/ihmwg/python-ihm/commit/206d96404b8cbd74a299127e86d1228f1df28210.patch?full_index=1"
-    sha256 "3a142c8cd5b3901271a5f892b8d4e40e2ba67d734b4516f1348814a19d9a4b2a"
-  end
 
   def install
     ENV.cxx11
