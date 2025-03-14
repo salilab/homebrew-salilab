250e5cb933204100238bf7c9cad9c6d4db0994c6
Ben Webb <ben@salilab.org>
2025-03-13 22:28:11 -0700

    Update to 2.3 release

--- ihm.rb
+++ ihm.rb
@@ -1,18 +1,18 @@
 class Ihm < Formula
   desc "Package for handling IHM mmCIF and BinaryCIF files"
   homepage "https://github.com/ihmwg/python-ihm"
-  url "https://pypi.io/packages/source/i/ihm/ihm-2.2.tar.gz"
-  sha256 "07967f323d1df12f81246b51b75e5910e54e8588e680be82709f0d347215a1f7"
+  url "https://pypi.io/packages/source/i/ihm/ihm-2.3.tar.gz"
+  sha256 "5a9955d7378da7670814972f3d6312a998e417371dbf433694d8295b9cf25796"
   license "MIT"
 
   bottle do
     root_url "https://salilab.org/homebrew/bottles"
-    sha256 arm64_sequoia: "ce0c14dff5250174608435bbb04e179b6dc5f67d3cfab02b3ca1c58592e4286f"
-    sha256 arm64_sonoma:  "682959023698c645f2f7b2210d192c89c7b93ff6510c004f54889a462a9c40be"
-    sha256 arm64_ventura: "66eda3c85cac23b00a03e06fb0451b47b84776b55f5fc4343be496ddd5c2d49f"
-    sha256 sequoia:       "f4fdc3dc076d299dfe0f57cc9d48d889c44d9e47d1497769d6cee3914c143065"
-    sha256 sonoma:        "26f3e80f6e78c7828f4f8d417e6fdea8e2cd9c3cb90cd112c79436224be8206a"
-    sha256 ventura:       "dd33aead6bffb07d399a8b79f1e33e096de1f7c190803dfca12362e7b14c83d8"
+    sha256 arm64_sequoia: "470dd3f290cad5b5c0a2af64d24a9fce322683b7f48a04d263a2f52576d039d2"
+    sha256 arm64_sonoma:  "bd41be0d5d7fb97246542f873a9509954d2bbc4f83c37ca1c4716f17321c9fc1"
+    sha256 arm64_ventura: "de74f4625936334a460a2d3bf13222a96fd1d0beebf95ca2aa834e2682cf99a9"
+    sha256 sequoia:       "337e4d3dbe2abbe1a318c1deb65b446d9d6993a37fcf6bd8e5c6d027da396dd0"
+    sha256 sonoma:        "0d62ac1e47ad5d2edc0220f658ae51ffdfefe8908a7243bbf3c884a8f0dc56b2"
+    sha256 ventura:       "563eabcfac1abc6aa39ed9f2c5699f0184ff4741fe2a9d7322b287d8e5e6cb6f"
   end
 
   depends_on "python@3.13"
