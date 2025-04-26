5ec0c8d8fa6b729c9e657ac6de135ae5d73a8aac
Ben Webb <ben@salilab.org>
2025-04-25 21:58:16 -0700

    Update for 2.5 release

--- ihm.rb
+++ ihm.rb
@@ -1,18 +1,18 @@
 class Ihm < Formula
   desc "Package for handling IHM mmCIF and BinaryCIF files"
   homepage "https://github.com/ihmwg/python-ihm"
-  url "https://pypi.io/packages/source/i/ihm/ihm-2.4.tar.gz"
-  sha256 "d9cfb54190f90d2449272d581ffa183cf75503bf48f9ee0f545c327dc3fbf299"
+  url "https://pypi.io/packages/source/i/ihm/ihm-2.5.tar.gz"
+  sha256 "de0723c8fa9039e5b752f02dfaa24b6eff02baeada57f5f44e953a4e6abb7b3a"
   license "MIT"
 
   bottle do
     root_url "https://salilab.org/homebrew/bottles"
-    sha256 arm64_sequoia: "99962885a32aad0ac73cd425e3a85c9d7440be0127d85dbed07ef6ca51a58a1a"
-    sha256 arm64_sonoma:  "095dd3ebd67df38fbd44500d575765f6662b47cb6a5b9874c57ddbf775983b84"
-    sha256 arm64_ventura: "fef5b7cb43b229f4f05062eae8f9060344587563ecb0d320d80f107da0d8be85"
-    sha256 sequoia:       "30ef6b83684fe8c6b618afa3e4422304f537a91c5018bc9b11a2a837c2b5773b"
-    sha256 sonoma:        "69df4ba2a953895105ceeceb8602c22c92e785d95c99ee2ac22e066c808a34ab"
-    sha256 ventura:       "3d65bfeb38c10e76154779d2f8c27c4f51e1202d2803cbc4d4353ad72fe754d6"
+    sha256 arm64_sequoia: "79b35013e16af710268f8795af55c7ff146e45ba4148a9d8675da0ad01cff58b"
+    sha256 arm64_sonoma:  "fbea70cadfdcbc5f802be83e7351b6c111db4b86e659f2f3c4a23bfb3da75f9a"
+    sha256 arm64_ventura: "c6a254a12d4fdea376e1d25fc897241772369ad930e347d94cacac05909f1ad7"
+    sha256 sequoia:       "b0b7da1540f6f7cf9767c74a6882ffc24972ee5917d53bcc82d562a673356518"
+    sha256 sonoma:        "0c3be17bcf4360ea43c9ed2d342ee932efc3b0d8a140e030cecd9c9bc53b6947"
+    sha256 ventura:       "bc2f4edcc03946f9dff3dabad5e84820a4043d3acdecb1faee53534cc4939af0"
   end
 
   depends_on "python@3.13"
@@ -33,6 +33,8 @@ class Ihm < Formula
       import ihm
       import ihm.dumper
       import ihm.reader
+      # Make sure the C extension built correctly
+      import ihm._format
       import os
 
       system = ihm.System(title='test system')
